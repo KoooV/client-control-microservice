745a4696dae21d1d655c3d3c066054642c6c99db
KoV <alexkovalev13681@gmail.com>
2025-06-20 20:54:22 +0300

    initial commit

---
+++ src/main/java/org/example/clientcontrolmicroservice/ClientControlMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.clientcontrolmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientControlMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientControlMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/clientcontrolmicroservice/ClientControlMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clientcontrolmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientControlMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
